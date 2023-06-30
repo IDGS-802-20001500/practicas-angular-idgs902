2c113414d1018bcd05413a29f091ede2ab35a995
IDGS-802-20001500 <mauricioisraelfer@gmail.com>
2023-06-29 23:14:53 -0600

    MIFR Clase 29/06/2023

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DistanciaPuntosComponent } from './Tarea/distancia-puntos/distancia-puntos.component';
+import { ResistenciasComponent } from './Tarea/resistencias/resistencias.component';
+import { CinepolisComponent } from './Tarea/cinepolis/cinepolis.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'distanciaPuntos', component: DistanciaPuntosComponent},
+  {path: 'resistencias', component: ResistenciasComponent },
+  {path: 'cinepolis', component: CinepolisComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
